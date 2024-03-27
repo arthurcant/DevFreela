d1ff1e697975d3e8b3568f2b6af9d0ed08f27cc0
arthurcant <arthurbig19@gmail.com>
2024-03-27 17:55:57 -0300

    Add project files.

---
+++ DevFreela.API/Program.cs
@@ -0,0 +1,46 @@
+using DevFreela.API.Models;
+using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Application.Services.Implementations;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DevFreelaCs");
+
+builder.Services.AddDbContext<DevFreelaDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddMediatR(typeof(CreateProjectCommand));
+
+builder.Services.AddSingleton<ExampleClass>(e => new ExampleClass { Name = "Initial Stage" });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ DevFreela.Core/Repositories/IProjectRepository.cs
@@ -0,0 +1,12 @@
+﻿using DevFreela.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Core.Repositories
+{
+    public interface IProjectRepository
+    {
+        Task<List<Project>> GetAll();
+    }
+}
